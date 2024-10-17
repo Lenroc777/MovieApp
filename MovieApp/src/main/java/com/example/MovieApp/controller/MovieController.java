7f4003e88dd84b7282e8b946e7ee2266d47a06ae
oliwiamanko <s97670@pollub.edu.pl>
2024-10-17 19:22:28 +0200

    crud/movie

---
+++ MovieApp/src/main/java/com/example/MovieApp/controller/MovieController.java
@@ -0,0 +1,48 @@
+package com.example.MovieApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    // GET: Zwraca wszystkie filmy
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    // GET: Zwraca film po ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        return movieService.getMovieById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // POST: Tworzy nowy film
+    @PostMapping
+    public Movie createMovie(@RequestBody Movie movie) {
+        return movieService.createMovie(movie);
+    }
+
+    // PUT: Aktualizuje film po ID
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movieDetails) {
+        return ResponseEntity.ok(movieService.updateMovie(id, movieDetails));
+    }
+
+    // DELETE: Usuwa film po ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        movieService.deleteMovie(id);
+        return ResponseEntity.noContent().build();
+    }
+}
