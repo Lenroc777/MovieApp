ecf001ed3f4b440e5b88ed0a5773e4c140241c69
Kornel <k.lugowski77@gmail.com>
2024-12-02 19:51:45 +0100

    feat/authorization
    fixed config

--- src/main/java/com/movieapp/movieapplication/config/SecurityConfig.java
+++ src/main/java/com/movieapp/movieapplication/config/SecurityConfig.java
@@ -39,6 +39,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+
+
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
